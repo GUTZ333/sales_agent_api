b4935f7e47430f68508c0221814a394310ae089f
GUTZ333 <gutz333lpsking32jotavve18@hotmail.com>
2026-02-20 14:44:56 -0300

    !

--- src/controllers/product_controller.ts
+++ src/controllers/product_controller.ts
@@ -6,7 +6,7 @@ const service = new ProductService()
 // O objeto de controle do pedidos que vai conter as interfaces das requisições HTTP e a lógica de negócio
 export class ProductController {
 
-  // 
+  // Handler que vai chama o service que tenta trazer todos os produtos avaliados
   async getAvailable(_: Request, res: Response) {
     const { data, error } = await service.getAvailable()
     if (error) return res.status(500).json(error)
@@ -14,6 +14,7 @@ export class ProductController {
     return res.json(data)
   }
 
+  // Handler que vai chama o service que tenta trazer um produto referenciado por um id
   async getById(req: Request, res: Response) {
     const { id } = req.params
     const { data, error } = await service.getById(id as string)
@@ -25,6 +26,7 @@ export class ProductController {
     return res.json(data)
   }
 
+   // Handler que vai chama o service que tenta trazer um produto referenciado pela categoria
   async search(req: Request, res: Response) {
     const { name } = req.query;
 
